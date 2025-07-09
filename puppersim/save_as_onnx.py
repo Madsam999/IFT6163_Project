c7fb59e321d1e7e8b935eb6e3e2bca30c8fc0142
Jacob Denault <jacosse1@gmail.com>
2025-07-09 17:09:19 -0400

    Update: Changed input and output names to match RL terms.

--- puppersim/save_as_onnx.py
+++ puppersim/save_as_onnx.py
@@ -30,10 +30,10 @@ def Convert_ONNX(output_path, obs_dim, agent):
          export_params=True,  # store the trained parameter weights inside the model file 
          opset_version=10,    # the ONNX version to export the model to 
          do_constant_folding=True,  # whether to execute constant folding for optimization 
-         input_names = ['modelInput'],   # the model's input names 
-         output_names = ['modelOutput'], # the model's output names 
-         dynamic_axes={'modelInput' : {0 : 'batch_size'},    # variable length axes 
-                                'modelOutput' : {0 : 'batch_size'}}) 
+         input_names = ['observation'],   # the model's input names 
+         output_names = ['action'], # the model's output names 
+         dynamic_axes={'observation' : {0 : 'batch_size'},    # variable length axes 
+                       'action' : {0 : 'batch_size'}}) 
     print('Model has been converted to ONNX')
     
 
